f8ced26736680f4f5fa3a099260a8a16628e8fd5
asmar <asmar-nasar2000@hotmail.com>
2022-01-11 19:10:16 +0100

    Ukesoppgave 1
    Ukesoppgavene til uke 1

---
+++ src/main/resources/static/javacript1/js1.js
@@ -0,0 +1,24 @@
+//Oppgave 1
+function heiVerden(){
+    document.write("Hei verden");
+}
+
+//Oppgave 2
+function heiVerden2() {
+    alert("Hei verden");
+}
+
+//Oppgave 3
+function inputChange(verdi) {
+    document.write(verdi);
+}
+
+//Oppgave 4
+function printAlert(verdi) {
+    alert(verdi);
+}
+
+//Oppgave 5
+function logAlert(verdi) {
+    console.log(verdi);
+}
