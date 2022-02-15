8144be236acda16e13e17a50a42557ca3b102022
asmar <asmar-nasar2000@hotmail.com>
2022-02-15 10:44:49 +0100

    Ukesoppgave 5 update
    Fikset opp validering i server i ekstraoppgaven. La inn punkt for uke 6 i index.html

--- src/main/java/com/asmar/webprog/klientTjener1/controller/KalkulatorController.java
+++ src/main/java/com/asmar/webprog/klientTjener1/controller/KalkulatorController.java
@@ -6,42 +6,62 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class KalkulatorController {
     @GetMapping("/pluss")
-    public double pluss(double tall1, double tall2) {
+    public double pluss(String tall1, String tall2) {
+        double utTall1;
+        double utTall2;
         try {
-            return tall1 + tall2;
+            utTall1 = Double.parseDouble(tall1);
+            utTall2 = Double.parseDouble(tall2);
         } catch(Exception e) {
             System.err.println("Feil fra server: " + e);
             return 0;
         }
+        return utTall1 + utTall2;
     }
 
     @GetMapping("/minus")
-    public double minus(double tall1, double tall2) {
+    public double minus(String tall1, String tall2) {
+        double utTall1;
+        double utTall2;
         try {
-            return tall1 - tall2;
+            utTall1 = Double.parseDouble(tall1);
+            utTall2 = Double.parseDouble(tall2);
         } catch(Exception e) {
             System.err.println("Feil fra server: " + e);
             return 0;
         }
+        return utTall1 - utTall2;
     }
 
     @GetMapping("/gange")
-    public double gange(double tall1, double tall2) {
+    public double gange(String tall1, String tall2) {
+        double utTall1;
+        double utTall2;
         try {
-            return tall1 * tall2;
+            utTall1 = Double.parseDouble(tall1);
+            utTall2 = Double.parseDouble(tall2);
         } catch(Exception e) {
             System.err.println("Feil fra server: " + e);
             return 0;
         }
+        return utTall1 * utTall2;
     }
 
     @GetMapping("/dele")
-    public double dele(double tall1, double tall2) {
+    public double dele(String tall1, String tall2) {
+        double utTall1;
+        double utTall2;
+        double resultat;
         try {
-            return tall1 / tall2;
+            utTall1 = Double.parseDouble(tall1);
+            utTall2 = Double.parseDouble(tall2);
+            if(utTall2 == 0 || utTall2 == 0.0) {
+                throw new ArithmeticException("Feil fra server: Kan ikke dele på 0!");
+            }
         } catch(Exception e) {
             System.err.println("Feil fra server: " + e);
             return 0;
         }
+        return utTall1 / utTall2;
     }
 }
