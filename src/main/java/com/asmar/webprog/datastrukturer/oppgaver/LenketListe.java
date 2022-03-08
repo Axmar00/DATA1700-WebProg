b493cb9e2900fef130cac8dbc0f2e17a5c8bc297
Asmar <asmar-nasar2000@hotmail.com>
2022-03-08 21:15:27 +0100

    Update LenketListe.java

--- src/main/java/com/asmar/webprog/datastrukturer/oppgaver/LenketListe.java
+++ src/main/java/com/asmar/webprog/datastrukturer/oppgaver/LenketListe.java
@@ -15,6 +15,7 @@ public class LenketListe {
     private Node hode = null;
     private Node hale = null;
 
+
     public void leggTilNode(int verdi)  {
         Node nyNode = new Node(verdi);
         if(hode == null) {
@@ -28,8 +29,37 @@ public class LenketListe {
         hale = nyNode;
     }
 
-    public void slettNode() {
+    public void slettNode(int slettVerdi) {
+        Node node = hode;
+        if(hode != null) {
+            // hvis listen ikke er tom
+            while (node != null) {
+                Node nesteNode = node.neste;
 
+                /*
+                if(hode == hale) {
+                // kun en node i listen
+                if(hode.verdi == slettVerdi) {
+                    hode = null;
+                    hale = null;
+                    return;
+                }
+                return;
+            }
+
+                if (node.verdi == slettVerdi) {
+                    // hvis hode skal slettes
+                    hode.neste = nesteNode.neste;
+                    hode = nesteNode;
+                    break;
+                } else if (hale.verdi == slettVerdi) {
+                    // hvis hale skal slettes
+
+                }*/
+                node = nesteNode;
+
+            }
+        }
     }
 
     public boolean inneholderNode(int sokeVerdi) {
@@ -52,6 +82,7 @@ public class LenketListe {
                 System.out.print(node.verdi + " ");
                 node = node.neste;
             }while(node != null);
+            System.out.println();
         }
     }
     public static void main(String[] args) {
@@ -59,6 +90,10 @@ public class LenketListe {
         minListe.leggTilNode(2);
         minListe.leggTilNode(4);
         minListe.leggTilNode(5);
-        System.out.println(minListe.inneholderNode(5));
+        minListe.leggTilNode(9);
+        minListe.leggTilNode(14);
+        minListe.skrivUtListe();
+        minListe.slettNode(2);
+        minListe.skrivUtListe();
     }
 }
