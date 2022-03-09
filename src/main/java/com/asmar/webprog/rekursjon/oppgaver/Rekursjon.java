5ef0e4d3402bb0dcad4150c1c184ebafe4191bc2
Asmar <asmar-nasar2000@hotmail.com>
2022-03-09 16:17:59 +0100

    Update Rekursjon.java

--- src/main/java/com/asmar/webprog/rekursjon/oppgaver/Rekursjon.java
+++ src/main/java/com/asmar/webprog/rekursjon/oppgaver/Rekursjon.java
@@ -1,28 +1,29 @@
 package com.asmar.webprog.rekursjon.oppgaver;
 
-public class TallOppgaver {
+public class Rekursjon {
     // rekursiv fakultet
     public static long fakultet(long tall) {
+        if(tall < 0) return -1;  // hvis tallet er negativt
         if(tall == 0) {
             return 1;
         }
-        else {
-            return tall * fakultet(tall - 1);
-        }
+        return tall * fakultet(tall - 1);
     }
 
     // rekursiv summasjon
     public static int sum(int tall) {
+        if(tall < 0) return -1; // hvis tallet er negativt
+
         if(tall == 0){
             return 0;
         }
-        else {
-            return tall + sum(tall-1);
-        }
+        return tall + sum(tall-1);
     }
 
     // iterativ summasjon
     public static int sum2(int tall) {
+        if(tall < 0) return -1; // hvis tallet er negativt
+
         int sum = 0;
         for(int i = tall; tall > 0; tall--){
             sum += tall;
